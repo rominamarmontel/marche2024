5bae1fc9f4c9a13d7babb21cca4fb3e3417e475a
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2024-06-23 17:26:57 +0200

    test succeeded

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -10,7 +10,6 @@ const ScrollToTop = () => {
   const [show, setShow] = useState(false)
 
   const changeShow = () => {
-    console.log('Window Scroll Y:', window.scrollY)
     if (window.scrollY > PAGE_Y_OFFSET) {
       setShow(true)
     } else {
@@ -24,15 +23,12 @@ const ScrollToTop = () => {
 
   useEffect(() => {
     window.addEventListener('scroll', changeShow)
-    console.log('Scroll Event Listener Added')
     return () => {
       window.removeEventListener('scroll', changeShow)
-      console.log('Scroll Event Listener Removed')
     }
   }, [])
 
   if (show) {
-    console.log('Show ScrollToTop Button')
     return (
       <div
         style={{ position: 'fixed', bottom: 20, right: 15, zIndex: 3 }}
@@ -50,7 +46,6 @@ const ScrollToTop = () => {
       </div>
     )
   } else {
-    console.log('Hide ScrollToTop Button')
     return null
   }
 }
